eab0afe73162e076e9e9bd8f4df389043f919928
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2024-09-02 21:03:13 +0300

    first commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default LandingPage
