407a806df51a43c8c56fd799094049df805908e0
Maigwa164 <davidmaigwa164@gmail.com>
2024-09-03 11:20:40 +0300

    (Feat)Added Features file

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,14 +1,13 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import Features from "../components/Features";
 
 function LandingPage() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-primaryOrange">
-        Hello world!
-      </h1>
+      <Features />
     </div>
-  )
+  );
 }
 
-export default LandingPage
+export default LandingPage;
