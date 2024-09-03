b00d3477d62250075e7fe8ecfc2f8dfe4013edd6
Anyona-00 <ombasaanyona@gmail.com>
2024-09-03 22:10:54 +0300

    (feat)Added the custom colours for the project

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 function LandingPage() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline text-primaryOrange">
+        Hello world!
+      </h1>
     </div>
   )
 }
