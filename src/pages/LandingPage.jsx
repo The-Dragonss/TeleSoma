1b90f20c7d7ab314a85a2e6e04acce3a79000739
Maigwa164 <davidmaigwa164@gmail.com>
2024-09-05 15:24:58 +0300

    (Feat)Added Feature cards

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,11 +3,7 @@ import React from "react";
 import Features from "../components/Features";
 
 function LandingPage() {
-  return (
-    <div>
-      <Features />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default LandingPage;
