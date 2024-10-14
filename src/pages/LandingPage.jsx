036b7c8d7847effffe05f139cd63637a9f799f92
Chang'ach <126554394+changach-3000@users.noreply.github.com>
2024-10-14 10:11:45 +0300

    add dashboard, signup and sign in, hero section

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import AboutUs from '../components/AboutUs'
+import Hero from '../components/Hero'
+import Sample from './Sample'
+import CoursePageBanner from './CourseBanner'
+import SignUpPage from './SignUp'
+import DashboardPage from './Dashboard'
 
 function LandingPage() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
+      {/* <h1 className="text-3xl font-bold underline">
       Hello world!
-    </h1>
+    </h1> */}
+    {/* <Hero />
+ <AboutUs /> */}
+ {/* <Sample /> */}
+{/* <SignUpPage /> */}
+<DashboardPage />
     </div>
   )
 }
